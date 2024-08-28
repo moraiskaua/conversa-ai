bb89616a420a7c1165a9d4af83c63a56308149d3
moraiskaua <moraiskaua03@gmail.com>
2024-08-28 04:29:41 -0300

    feat: config theme context provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Plus_Jakarta_Sans } from 'next/font/google';
 import './globals.css';
 import { ClerkProvider } from '@clerk/nextjs';
 import { Toaster } from '@/components/ui/toaster';
+import { ThemeProvider } from '@/contexts/theme-provider';
 
 const jakarta = Plus_Jakarta_Sans({ subsets: ['latin'] });
 
@@ -21,8 +22,14 @@ export default function RootLayout({
       <html lang="pt-BR">
         <link rel="shortcut icon" href="/images/logo.png" type="image/x-icon" />
         <body className={jakarta.className}>
-          {children}
-          <Toaster />
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="light"
+            disableTransitionOnChange
+          >
+            {children}
+            <Toaster />
+          </ThemeProvider>
         </body>
       </html>
     </ClerkProvider>
