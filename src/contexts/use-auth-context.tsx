165440ebb880bcbcf555c1cbf0a68f3b8ac2bd69
moraiskaua <moraiskaua03@gmail.com>
2024-08-28 01:29:33 -0300

    feat: create auth context

---
+++ src/contexts/use-auth-context.tsx
@@ -0,0 +1,34 @@
+import {
+  Dispatch,
+  ReactNode,
+  SetStateAction,
+  createContext,
+  useContext,
+  useState,
+} from "react";
+
+interface InitialValues {
+  currentStep: number;
+  setCurrentStep: Dispatch<SetStateAction<number>>;
+}
+
+const initialValues: InitialValues = {
+  currentStep: 1,
+  setCurrentStep: () => undefined,
+};
+
+const AuthContext = createContext(initialValues);
+
+export const AuthContextProvider = ({ children }: { children: ReactNode }) => {
+  const [currentStep, setCurrentStep] = useState<number>(
+    initialValues.currentStep
+  );
+
+  return (
+    <AuthContext.Provider value={{ currentStep, setCurrentStep }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuthContext = () => useContext(AuthContext);
